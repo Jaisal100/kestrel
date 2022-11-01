c4695e50b0a79325e6779e110588d4c5c2ca0043
0day <70134418+Jaisal100@users.noreply.github.com>
2022-11-01 16:56:21 -0400

    Changed IPv4 Scanner to account for invalid IPs. Added a URL scanner.

--- IPv4_Scanner.py
+++ IPv4_Scanner.py
@@ -14,20 +14,21 @@ response = requests.get(url, headers=headers)
 data=response.text
 
 parse_json=json.loads(data)
-print(parse_json)
 
-IPv4_report=parse_json['data']['attributes']['last_analysis_stats']
+if 'error' not in parse_json:
+    IPv4_report=parse_json['data']['attributes']['last_analysis_stats']
+    harmless_stat=IPv4_report['harmless']
+    undetected_stat=IPv4_report['undetected']
+    suspicious_stat=IPv4_report['suspicious']
+    malicious_stat=IPv4_report['malicious']
 
-harmless_stat=IPv4_report['harmless']
-undetected_stat=IPv4_report['undetected']
-suspicious_stat=IPv4_report['suspicious']
-malicious_stat=IPv4_report['malicious']
-
-if malicious_stat > 0:
-    print("This IP is a known bad actor.")
+if 'error' in parse_json:
+    print("IPv4 address is invalid.")
+elif malicious_stat > 0:
+    print("This IPv4 address is a known bad actor.")
 elif harmless_stat > 0 and malicious_stat == 0 and suspicious_stat == 0:
-    print("This IP is not a known bad actor.")
+    print("This IPv4 address is not a known bad actor.")
 elif undetected_stat > 0:
-    print("The threat level of this IP is currrently unknown.")
+    print("The threat level of this IPv4 address is currrently unknown.")
 else:
-    print("This IP is potentially from a bad actor.")
+    print("This IPv4 address is potentially from a bad actor.")
---
+++ URL_Scanner.py
@@ -0,0 +1,36 @@
+import requests
+import json
+import base64
+
+url_=input("Please enter a URL: ")
+url_id = base64.urlsafe_b64encode("{}".format(url_).encode()).decode().strip("=")
+API_Key=input("Please enter a VirusTotal API key: ")
+url = "https://www.virustotal.com/api/v3/urls/{}".format(url_id)
+headers = {
+    "accept": "application/json",
+    "x-apikey": "{}".format(API_Key)
+}
+
+response = requests.get(url, headers=headers)
+
+data=response.text
+
+parse_json=json.loads(data)
+
+if 'error' not in parse_json:
+    URL_report=parse_json['data']['attributes']['last_analysis_stats']
+    harmless_stat=URL_report['harmless']
+    undetected_stat=URL_report['undetected']
+    suspicious_stat=URL_report['suspicious']
+    malicious_stat=URL_report['malicious']
+
+if 'error' in parse_json:
+    print("URL is invalid.")
+elif malicious_stat > 0:
+    print("This URL is a known bad actor.")
+elif harmless_stat > 0 and malicious_stat == 0 and suspicious_stat == 0:
+    print("This URL is not a known bad actor.")
+elif undetected_stat > 0:
+    print("The threat level of this URL is currrently unknown.")
+else:
+    print("This URL is potentially from a bad actor.")
